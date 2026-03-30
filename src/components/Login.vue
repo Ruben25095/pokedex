2221d8e9e1bf7ed1a0da107cecc07c5e58696c7c
Ruben25095 <rubens25095@gmail.com>
2026-03-29 20:07:29 -0700

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,31 @@
 <template>
-    <div class="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-green-400 to-blue-500">
-        <h1>Bienvenido a Rutas 2026</h1>
-        <p>Login</p>
+    <div class="flex  flex-col items-center  h-screen bg-gradient-to-br from-green-400 to-blue-500">
+        
+         <div class="flex  mt-48 rounded-lg min-w-1/4 min-h-1/3 flex-col items-center bg-gradient-to-br from-black-500 to-gray-600 pb-10" >
+            <h1 class="mt-5 text-3xl text-white">Iniciar Sesion</h1>
+
+            <label class="mt-8 text-white">Ingrese correo</label>
+            <input type="email" id="email" placeholder="Ingrese correo" class="  bg-white p-2 rounded-xl mb-4 min-w-1/2">
+
+
+              <label class="mt-2 text-white">Ingrese contraseña</label>
+            <input  type="password" id="email" placeholder="contraseña" class="  p-2 bg-white  rounded-xl mb-4 min-w-1/2">
+
+
+            <button class="bg-gradient-to-br from-green-500 to-blue-600 text-white px-6 py-2 rounded hover:from-green-600 hover:to-blue-700 transition-colors">
+              Iniciar Sesion
+              
+            </button>
+
+         </div>
+
+
+
+
+
+
+
+
        
     </div>  
 </template>
