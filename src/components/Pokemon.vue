5ad76eb383ca05701fba4b68177137ffc3c1e68f
Ruben25095 <rubens25095@gmail.com>
2026-03-29 20:12:36 -0700

    Nombre pokemon Pokemon.vue

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -90,10 +90,11 @@ const Anterior = () => {
               :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonid(pokemon.url)}.png`" 
               :alt="pokemon.name"
             />
-          </div>
-          <p class="mt-2 font-bold capitalize text-white text-sm drop-shadow-md">
+              <p class="mt-2 font-bold capitalize text-white text-sm drop-shadow-md">
             {{ pokemon.name }}
           </p>
+          </div>
+        
         </div>
       </div>
     </div>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -4,7 +4,6 @@
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png" alt="Pokemon Image">
 
 
-       <h1 class="text-2xl font-bold">Nombre del Pokemon</h1>
 
 
   </div>
